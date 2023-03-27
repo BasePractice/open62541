525348f8562b8fc2677b252341a9f95ed4f2bf29
Хлебников Андрей Александрович <Andrei.Khlebnikov@domrf.ru>
2023-03-27 22:34:06 +0300

    New Raspberry PI PICO arch

---
+++ arch/pico/ua_architecture.h
@@ -0,0 +1,36 @@
+#ifdef UA_ARCHITECTURE_PICO
+
+#ifndef PLUGINS_ARCH_PICO_UA_ARCHITECTURE_H_
+#define PLUGINS_ARCH_PICO_UA_ARCHITECTURE_H_
+
+#include "ua_lwip.h"
+#include "ua_freeRTOS.h"
+
+#if UA_MULTITHREADING >= 100
+#error Multithreading unsupported
+#else
+#define UA_LOCK_INIT(lock)
+#define UA_LOCK_DESTROY(lock)
+#define UA_LOCK(lock)
+#define UA_UNLOCK(lock)
+#define UA_LOCK_ASSERT(lock, num)
+#endif
+
+#define UA_strncasecmp strncasecmp
+
+// freeRTOS does not have getifaddr
+#undef UA_HAS_GETIFADDR
+
+#ifndef IN6_IS_ADDR_UNSPECIFIED
+# define IN6_IS_ADDR_UNSPECIFIED(a) \
+        (((const uint32_t *) (a))[0] == 0                                      \
+         && ((const uint32_t *) (a))[1] == 0                                      \
+         && ((const uint32_t *) (a))[2] == 0                                      \
+         && ((const uint32_t *) (a))[3] == 0)
+#endif
+
+#include <open62541/architecture_functions.h>
+
+#endif /* PLUGINS_ARCH_PICO_UA_ARCHITECTURE_H_ */
+
+#endif /* UA_ARCHITECTURE_PICO */
